f854a6f1bc87230ba5a0be50c3df4b45e076ba54
Tamok <nautiluce@gmail.com>
2023-07-03 17:26:54 -0700

    Fixed errors in main.rs based on Dioxus documentation

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,17 @@
 // This is the main entry point for the ToDoWin application.
 // It initializes the Dioxus application and starts the event loop.
 
-use dioxus::core::component::Component;
-use dioxus::core::context::Context;
-use dioxus::core::element::Element;
-use dioxus::html::div;
-use dioxus::runtime::start;
+use dioxus::prelude::*;
 
-fn main() {
-    // Start the Dioxus application with the App component.
-    start::<App>();
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
 }
 
-struct App;
-
-impl Component for App {
-    fn render(&self, _cx: &mut Context) -> Element {
-        // The app's main UI goes here
-        // For now, let's just display a simple message
-        div().text("Welcome to ToDoWin!").into()
-    }
+fn main() {
+    // TODO: Start your app. You'll need to use a Dioxus renderer for this.
+    // The exact code will depend on the renderer you're using.
 }
