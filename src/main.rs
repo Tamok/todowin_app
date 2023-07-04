1e150aa527efe1e7d2d15d0b1adde8a979e2a17a
Tamok <nautiluce@gmail.com>
2023-07-03 17:18:07 -0700

    Fix import paths in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 // This is the main entry point for the ToDoWin application.
 // It initializes the Dioxus application and starts the event loop.
 
-use dioxus::prelude::*;
+use dioxus::core::component::Component;
+use dioxus::core::context::Context;
+use dioxus::core::element::Element;
+use dioxus::html::div;
+use dioxus::runtime::start;
 
 fn main() {
     // Start the Dioxus application with the App component.
-    dioxus::start::<App>();
+    start::<App>();
 }
 
 struct App;
@@ -15,8 +19,6 @@ impl Component for App {
     fn render(&self, _cx: &mut Context) -> Element {
         // The app's main UI goes here
         // For now, let's just display a simple message
-        div()
-            .text("Welcome to ToDoWin!")
-            .into()
+        div().text("Welcome to ToDoWin!").into()
     }
 }
